18219ec1edbf5e9b9e814e00654fc9165e728864
Catharina Mesquita <catha.ana.1994@gmail.com>
2022-02-18 17:30:53 -0300

    fix: add link to repo in github

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,3 +1,3 @@
 export function Footer() {
-  return <footer>Catharina Mesquita - 2022 - <a href="#" className="footer">Github</a></footer>
+  return <footer>Catharina Mesquita - 2022 - <a href="https://github.com/acmesquita/candy-love-remix" target="_blank" className="footer">Github</a></footer>
 }
