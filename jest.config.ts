468e143dcd9e8383ee7e60b93c85582dd5721f49
Catharina Mesquita <catha.ana.1994@gmail.com>
2022-03-19 12:15:32 -0300

    refactor: move cypress tests to e2e folder

--- jest.config.ts
+++ jest.config.ts
@@ -157,7 +157,7 @@ export default {
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: [
     "/node_modules/",
-    "/cypress/"
+    "/tests/e2e/"
   ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
